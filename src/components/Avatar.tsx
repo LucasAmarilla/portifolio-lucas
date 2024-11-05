057636d82a862bc2c4e0aa26b04babe9c9612467
LucasAmarilla <amarilla482@gmail.com>
2024-11-05 00:55:14 -0400

    Changes to be committed: 	modified:   src/components/Avatar.tsx 	modified:   src/components/Nav.tsx 	modified:   tsconfig.json 	modified:   vite.config.ts

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,7 +2,7 @@ import photo from '../assets/photo.png'
 
 export default function Avatar() {
     return <>
-        <div class="rounded-full h-[20rem] w-[20rem] bg-red-600 overflow-hidden m-auto">
+        <div class="rounded-full h-[20rem] w-[20rem] bg-sky-600 overflow-hidden m-auto">
             <img src={photo} alt="" srcset="" class='h-full w-full object-cover' />
         </div>
     </>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ export default function Nav(props: any) {
 
         <div class="flex items-center absolute left-1/2 -translate-x-1/2 rounded-full bg-white/75 bg-gradient-to-r from-sky-200/40 via-sky-200/40 to-sky-200/40 border border-white/50 px-3 text-sm font-medium text-gray-800 shadow-lg shadow-gray-800/5 ring-1 ring-gray-800/[.075] backdrop-blur-xl my-4">
 
-            <A class="flex-none group relative block transition duration-300 px-3 py-2.5 hover:text-sky-600" href="https://lucasamarilla.github.io/portifolio-lucas/">
+            <A class="flex-none group relative block transition duration-300 px-3 py-2.5 hover:text-sky-600" href="/">
                 Me
                 <span class="absolute inset-x-1 h-px bg-gradient-to-r from-sky-500/0 from-10% via-sky-400 to-sky-500/0 to-90% transition duration-300 -bottom-0.5 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100">
                 </span>
@@ -15,7 +15,7 @@ export default function Nav(props: any) {
                     </span>
                 </span>
             </A>
-            <a class="flex-none group relative block transition duration-300 px-3 py-2.5 hover:text-sky-600" href="https://lucasamarilla.github.io/portifolio-lucas/projects">
+            <a class="flex-none group relative block transition duration-300 px-3 py-2.5 hover:text-sky-600" href="/projects">
                 My projects
                 <span class="absolute inset-x-1 h-px bg-gradient-to-r from-sky-500/0 from-10% via-sky-400 to-sky-500/0 to-90% transition duration-300 -bottom-0.5 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100">
                 </span>
